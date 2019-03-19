cf58b63b5d53a57e7a8c5d2b74978298fca8df79
John Easton <dev@easton.dev>
2019-03-19 11:36:19 +0000

    add error

--- functions/bankbar-proxy.js
+++ functions/bankbar-proxy.js
@@ -1,4 +1,15 @@
 exports.handler = async (event) => {
+  if (!event.queryStringParameters.code) {
+    const html = `Oops! <br /> No auth code found in the request. <br /> Try logging in again by restarting bankBar from the dropdown menu. <br /> <br /> Get in touch if the issue persists. <br /> @johneas10`
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: html,
+    }
+  }
+
   return {
     statusCode: 301,
     headers: {
