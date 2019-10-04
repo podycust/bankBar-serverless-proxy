c3f89fa96864b052b6942741aeb8fcfcf2e35807
podycust <pgr3master@gmail.com>
2019-10-04 15:59:34 +0100

    Update and rename bankbar-proxy.js to mdesktop-proxy.js

--- functions/mdesktop-proxy.js
+++ functions/mdesktop-proxy.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   if (!event.queryStringParameters.code) {
-    const html = `Oops! <br /> No auth code found in the request. <br /> Try logging in again by restarting bankBar from the dropdown menu. <br /> <br /> Get in touch if the issue persists. <br /> @johneas10`
+    const html = `Oops! <br /> No auth code found in the request. <br /> Try logging in again by restarting Monzo Desktop from the dropdown menu. <br /> <br /> Get in touch if the issue persists. <br /> @johneas10`
     return {
       statusCode: 500,
       headers: {
