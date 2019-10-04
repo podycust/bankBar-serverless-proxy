3b05f6afd4b2454495f802f775f16b043c0223c0
podycust <pgr3master@gmail.com>
2019-10-04 15:51:11 +0100

    Update bankbar-proxy.js
    m

--- functions/bankbar-proxy.js
+++ functions/bankbar-proxy.js
@@ -13,7 +13,7 @@ exports.handler = async (event) => {
   return {
     statusCode: 301,
     headers: {
-      Location: `bankbar://${event.queryStringParameters.code}`,
+      Location: `mdesktop://${event.queryStringParameters.code}`,
       'Cache-Control': 'no-cache'
     }
   }
